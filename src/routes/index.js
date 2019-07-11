19444cf0ad85a50ba8df6032df3559360259b33a
Juan Camilo Arango <Camilo@MacBook-Pro-de-usuario.local>
2019-07-11 17:58:37 -0500

    Registrar estudiante en bd mongo

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const hbs = require('hbs');
 const Course = require('./../models/course');
+const Student = require('./../models/student');
 const dirPartials = path.join(__dirname, '../../template/partials');
 const dirViews = path.join(__dirname, '../../template/views/');
 
@@ -25,6 +26,30 @@ app.get('/formNewCourse', (req, res) => {
   });
 });
 
+app.get('/formCourses', (req, res) => {
+  res.render(dirViews + 'formCourses', {
+
+  });
+});
+
+app.get('/formRegister', (req, res) => {
+  res.render(dirViews + 'formRegisterStudent', {
+    idCourse: req.query.idCourse
+  });
+});
+
+app.get('/formStudentsByCourse', (req, res) => {
+  res.render(dirViews + 'formStudentsByCourse', {
+    idCourse: req.query.idCourse
+  });
+});
+
+app.get('*', (req, res) => {
+  res.render(dirViews + 'error', {
+    estudiante: 'error'
+  });
+});
+
 app.post('/createCourse', (req, res) => {
 
   let course = new Course({
@@ -43,9 +68,9 @@ app.post('/createCourse', (req, res) => {
       })
     }
     res.render(dirViews + 'index', {
-      myTitle: result
+      myTitle: 'Course ' + name + ' created successfully!'
     })
-  }) 
+  })
 
 /*
   res.render(dirViews + 'createCourse', {
@@ -60,38 +85,36 @@ app.post('/createCourse', (req, res) => {
   */
 });
 
-app.get('/formCourses', (req, res) => {
-  res.render(dirViews + 'formCourses', {
-
-  });
-});
-
-app.get('/formRegister', (req, res) => {
-  res.render(dirViews + 'formRegisterStudent', {
-    idCourse: req.query.idCourse
-  });
-});
-
 app.post('/registerStudent', (req, res) => {
-  res.render(dirViews + 'registerStudent', {
+  let student = new Student({
     idCourse: req.body.idCourse,
     documentId: req.body.documentId,
     name: req.body.name,
     email: req.body.email,
     telephone: req.body.telephone
   });
+
+  student.save((err, result) => {
+    if (err) {
+      res.render(dirViews + 'index', {
+        myTitle: err
+      })
+    }
+    res.render(dirViews + 'index', {
+      myTitle: 'The student ' + name + ' has been registered in course ' + idCourse
+    })
+  });
+
+  /*res.render(dirViews + 'registerStudent', {
+    idCourse: req.body.idCourse,
+    documentId: req.body.documentId,
+    name: req.body.name,
+    email: req.body.email,
+    telephone: req.body.telephone
+  });
+  */
 });
 
-app.get('/formStudentsByCourse', (req, res) => {
-  res.render(dirViews + 'formStudentsByCourse', {
-    idCourse: req.query.idCourse
-  });
-});
 
-app.get('*', (req, res) => {
-  res.render(dirViews + 'error', {
-    estudiante: 'error'
-  });
-});
 
 module.exports = app;
