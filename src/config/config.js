f98b0ba628eff4e05846502696ce491e8b5e5d1a
juan arango <juancamiloarango@gmail.com>
2019-07-22 20:15:40 -0500

    variable urlbd

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,6 @@
 process.env.PORT = process.env.PORT || 3000;
+process.env.NODE_ENV = process.env.NODE_ENV || 'local';
+
 //process.env.URLDB = 'mongodb://localhost:27017/coursesManager'
 
 let urlDB
