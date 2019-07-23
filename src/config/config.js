028945b255124eef7cbea4bbeb31ae8bb8c2b5ef
juan arango <juancamiloarango@gmail.com>
2019-07-22 20:11:38 -0500

    variable urlbd

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,12 @@
 process.env.PORT = process.env.PORT || 3000;
-process.env.URLDB = 'mongodb://localhost:27017/coursesManager'
+//process.env.URLDB = 'mongodb://localhost:27017/coursesManager'
+
+let urlDB
+if (process.env.NODE_ENV === 'local'){
+	urlDB = 'mongodb://localhost:27017/coursesManager';
+}
+else
+	urlDB = 'mongodb+srv://jca-admin:jca87admin@cluster0-zfyfv.mongodb.net/coursesManager?retryWrites=true'
+}
+
+process.env.URLDB = urlDB
